32bdc0e2d31633e4d50359458e1a7b68ba7ca21e
julialonghi <116280754+julialonghi@users.noreply.github.com>
2026-03-21 18:38:09 -0300

    Add files via upload

---
+++ Exemplo6.java
@@ -0,0 +1,25 @@
+// Júlia Oliveira Longhi
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Exemplo6 {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            AtomicLong totalTempo = new AtomicLong(0);
+            br.lines()
+                .takeWhile(linha -> !linha.trim().equals("FIM"))
+                .map(linha -> linha.split("\\|"))
+                .map(v -> AccessLog.from(v[0].trim(), v[1].trim(), v[2].trim(), v[3].trim(), v[4].trim(), v[5].trim()))
+                .filter(log -> log.metodo().equalsIgnoreCase("PUT"))
+                .peek(log -> {
+                    totalTempo.addAndGet(log.tempoResposta());
+                    System.out.println("Total parcial (PUT): " + totalTempo.get());
+                })
+                .count();
+            System.out.println("Total final: " + totalTempo.get());
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
